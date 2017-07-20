414018c9586494edc4a54bfa36dcc444103dac89
Mariano Tucat <mariano@skedgo.com>
2017-07-20 13:03:08 -0300

    remove random workerID to enable recover after crashing/stopped process

--- src/main/java/com/amazonaws/services/dynamodbv2/streams/connectors/CommandLineInterface.java
+++ src/main/java/com/amazonaws/services/dynamodbv2/streams/connectors/CommandLineInterface.java
@@ -1,13 +1,13 @@
 /*
  * Copyright 2014-2015 Amazon.com, Inc. or its affiliates. All Rights Reserved.
- * 
+ *
  * Licensed under the Amazon Software License (the "License"). You may not use this file except in compliance with the License.
  * A copy of the License is located at
- * 
+ *
  * http://aws.amazon.com/asl/
- * 
+ *
  * or in the "LICENSE.txt" file accompanying this file.
- * 
+ *
  * This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, express or implied.
  * See the License for the specific language governing permissions and limitations under the License.
  */
@@ -211,7 +211,7 @@ public class CommandLineInterface {
         final KinesisClientLibConfiguration kclConfig = new KinesisClientLibConfiguration(actualTaskName,
                 streamArn,
                 credentialsProvider,
-                DynamoDBConnectorConstants.WORKER_LABEL + actualTaskName + UUID.randomUUID().toString())
+                DynamoDBConnectorConstants.WORKER_LABEL + actualTaskName)
                 // worker will use checkpoint table if available, otherwise it is safer
                 // to start at beginning of the stream
                 .withInitialPositionInStream(InitialPositionInStream.TRIM_HORIZON)
